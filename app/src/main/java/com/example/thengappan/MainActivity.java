5cbfbd8447133d88c39061754e1095f68071eb1c
CodeBrigade6 <CodeBrigade2023@gmail.com>
2023-07-11 11:57:48 +0530

    Theme Updated

--- app/src/main/java/com/example/thengappan/MainActivity.java
+++ app/src/main/java/com/example/thengappan/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.thengappan;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,8 +12,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
+        setContentView(R.layout.activity_main);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
