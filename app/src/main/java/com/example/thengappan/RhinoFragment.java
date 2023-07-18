9ec5923d4eed3b6160a75f119373846b3446a79d
NAVEEN RAJ <84392589+naveenraj2224@users.noreply.github.com>
2023-07-18 13:48:11 +0530

    Rhino Fragment Created

--- app/src/main/java/com/example/thengappan/RhinoFragment.java
+++ app/src/main/java/com/example/thengappan/RhinoFragment.java
@@ -7,6 +7,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +17,7 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class RhinoFragment extends Fragment {
+
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -25,6 +29,7 @@ public class RhinoFragment extends Fragment {
     private String mParam2;
 
     public RhinoFragment() {
+   
         // Required empty public constructor
     }
 
@@ -44,6 +49,7 @@ public class RhinoFragment extends Fragment {
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
+
     }
 
     public static RhinoFragment newInstance() {
@@ -58,12 +64,16 @@ public class RhinoFragment extends Fragment {
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
+
+
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_rhino, container, false);
     }
